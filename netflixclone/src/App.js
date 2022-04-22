276289afaa38c9c8b00577ebac574d66af816b24
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-04-22 20:30:21 -0300

    criação da animação ao passar o mouse em cima do filme (hover) e criação do filme em destaque (ainda deve ser implementado)

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow'
 import './App.css'
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
 
     const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedData] = useState(null);
 
     useEffect(() => {
       const loadAll = async () => {
@@ -17,6 +19,11 @@ export default () => {
 
     return (
       <div className="page">
+
+        {featuredData &&
+          <FeaturedMovie item={featuredData} />
+        }
+         
           <section className="lists">
             {movieList.map((item, key) => (
               <MovieRow key={key} title={item.title} items={item.items}/>
