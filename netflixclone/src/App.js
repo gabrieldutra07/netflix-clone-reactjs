bb3d78a9f8e0638a06250858b97b6e58781c9db8
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-04-23 00:39:29 -0300

    mais implementações na página inicial

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -13,6 +13,13 @@ export default () => {
       const loadAll = async () => {
           let list = await Tmdb.getHomeList();
           setMovieList(list)
+
+          let originals = list.filter(i => i.slug === 'originals');
+          let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1))
+          let chosen = originals[0].items.results[randomChosen];
+          let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+          setFeaturedData(chosenInfo);
         }
       loadAll()
     }, [])
--- netflixclone/src/components/FeaturedMovie.js
+++ netflixclone/src/components/FeaturedMovie.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './FeaturedMovie.css';
 
-export default () => {
+export default ({item}) => {
     return (
-        <div>
-            FILME EM DESTAQUE
-        </div>
+
+        <section className='featured' style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+
+            <div className='featured--vertical'>
+                <div className='featured-horizontal'>
+                    <div className='featured--name'>
+                        {item.original_name}
+                        <div className='featured--info'>
+                            <div className='featured--points'>{item.vote_average}
+                            </div>
+                            <div className='featured--year'>
+                                    2099
+                             </div>
+                             <div className='featured--seasons'>
+                                 {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}
+                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
     )
 }
