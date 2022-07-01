fbe20cdbc2ba02acfc224269a81509cf82877a00
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-06-30 21:18:05 -0300

    capturada todas as informações do filme (nome, temporada, ano, genero, etc)

--- netflixclone/src/components/FeaturedMovie.js
+++ netflixclone/src/components/FeaturedMovie.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './FeaturedMovie.css';
 
 export default ({item}) => {
+    let firstDate = new Date(item.first_air_date);
+    let genres = []
+
+    for(let i in item.genres) {
+        genres.push(item.genres[i].name)
+    }
+
     return (
 
         <section className='featured' style={{
@@ -18,12 +25,18 @@ export default ({item}) => {
                             <div className='featured--points'>{item.vote_average}
                             </div>
                             <div className='featured--year'>
-                                    2099
+                                    {firstDate.getFullYear}
                              </div>
                              <div className='featured--seasons'>
                                  {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''} 
                              </div>
                         </div>
+                        <div className='featured--description'>{item.overview}</div>^
+                        <div className='featured--buttons'>
+                            <a href={`/watch/${item.id}`}>Assistir</a>
+                            <a href={`/list/add/${item.id}`}>+ Minha Lista</a>
+                        </div>
+                        <div className='featured--genres'><strong>Gêneros:</strong> {genres.join(', ')}</div>
                     </div>
                 </div>
             </div>
